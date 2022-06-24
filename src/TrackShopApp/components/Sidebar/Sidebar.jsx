20caffc6f4cf15b5aff35add165351bf3c0a41e9
Abdalla <aboode114g2@gmail.com>
2022-06-24 21:53:15 +0300

    React Track app sidebar

---
+++ src/TrackShopApp/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import "./Sidebar.css";
+import Logo from "../../imgs/gimg.png";
+//https://react-icons.github.io/react-icons/
+import { FaBeer } from "react-icons/fa";
+// https://iconscout.com/unicons/explore/line
+//Note: you can add more than icon with ,
+import {
+  UilHome,
+  UilEstate,
+  UilSignOutAlt,
+  UilBars
+} from "@iconscout/react-unicons";
+import { SidebarData } from "../../../Data/Data";
+
+const Sidebar = () => {
+  const [selected, setSelected] = useState(0);
+  const [expand, setExpand] = useState(true);
+  return (
+    <>
+      <div className="bar">
+        <UilBars
+          onClick={() => setExpand(!expand)}
+          style={{ cursor: "pointer", background: "" }}
+        />
+      </div>
+      <div className={("Sidebar", expand ? "sidebarMobile" : "sidebarMoHide")}>
+        {/* Logo */}
+        <div className="logo">
+          <img className="img24px" src={Logo} alt="" />
+          <span>
+            L<span>o</span>go
+          </span>
+        </div>
+
+        {/* H menu */}
+        <div className="menu">
+          {SidebarData.map((item, index) => {
+            return (
+              <div
+                className={selected === index ? "menuItem active" : "menuItem"}
+                key={index}
+                onClick={() => setSelected(index)}
+              >
+                <item.icon />
+                <span>{item.heading}</span>
+              </div>
+            );
+          })}
+
+          <div className="menuItem">
+            <UilSignOutAlt />
+          </div>
+
+          {/* the bottom div for
+div menu
+div sidebar
+
+ */}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
