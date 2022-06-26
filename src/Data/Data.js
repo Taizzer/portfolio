a04ec217d91a0bd816f0b5d73c95b7e27f8fde99
Abdalla <aboode114g2@gmail.com>
2022-06-26 14:27:47 +0300

    background color changes

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -5,39 +5,39 @@ import {
   UilTag,
   UilShop,
   UilShoppingBag,
-  UilStore
+  UilStore,
 } from "@iconscout/react-unicons";
 
 //Sidebar Data
 export const SidebarData = [
   {
     icon: UilHome,
-    heading: "Dashboard"
+    heading: "Dashboard",
   },
   {
     icon: UilTag,
-    heading: "Orders"
+    heading: "Orders",
   },
   {
     icon: UilShop,
-    heading: "Customers"
+    heading: "Customers",
   },
   {
     icon: UilShoppingBag,
-    heading: "Products"
+    heading: "Products",
   },
   {
     icon: UilStore,
-    heading: "Analytics"
-  }
+    heading: "Analytics",
+  },
 ];
 
 export const CardData = [
   {
     title: "sales",
     color: {
-      backGround: "hsl(0, 0%, 92%)",
-      boxShadow: "0px 5px 5px 0px #c3c3c3"
+      backGround: "hsl(0, 0%, 95%)",
+      boxShadow: "0px 5px 5px 0px #c3c3c3",
     },
     barValue: 70,
     value: "111",
@@ -45,15 +45,15 @@ export const CardData = [
     series: [
       {
         name: "Sales",
-        data: [11, 14, 44, 77, 22, 45, 33]
-      }
-    ]
+        data: [11, 14, 44, 77, 22, 45, 33],
+      },
+    ],
   },
   {
     title: "Revenue",
     color: {
-      backGround: "hsl(0,0%,92%)",
-      boxShadow: "0px 5px 5px 0px #c3c3c3"
+      backGround: "hsl(0,0%,95%)",
+      boxShadow: "0px 5px 5px 0px #c3c3c3",
     },
     barValue: 90,
     value: "66,22",
@@ -61,15 +61,15 @@ export const CardData = [
     series: [
       {
         name: "Revenue",
-        data: [2, 4, 12, 22, 33, 44, 88]
-      }
-    ]
+        data: [2, 4, 12, 22, 33, 44, 88],
+      },
+    ],
   },
   {
     title: "Expenses",
     color: {
-      backGround: "hsl(0, 0%, 92%)",
-      boxShadow: "0px 5px 5px 0px #c3c3c3"
+      backGround: "hsl(0, 0%, 95%)",
+      boxShadow: "0px 5px 5px 0px #c3c3c3",
     },
     barValue: 50,
     value: "28,22",
@@ -77,10 +77,10 @@ export const CardData = [
     series: [
       {
         name: "sales",
-        data: [11, 22, 44, 88, 66, 44, 4]
-      }
-    ]
-  }
+        data: [11, 22, 44, 88, 66, 44, 4],
+      },
+    ],
+  },
 ];
 
 // update reviews
@@ -93,20 +93,20 @@ export const UpdateReview = [
     img: img1,
     note: "orderd Apple Iphone12",
     name: "Tom cruiz ",
-    time: "10 minutes ago"
+    time: "10 minutes ago",
   },
 
   {
     img: img2,
     note: "orderd Samsung Galaxy s22",
     name: "Keanu Reeves ",
-    time: "1 hour ago"
+    time: "1 hour ago",
   },
 
   {
     img: img3,
     note: "orderd MacBook",
     name: "Johnny Depp",
-    time: "1 hour ago"
-  }
+    time: "1 hour ago",
+  },
 ];
