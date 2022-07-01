9e3c5f345f3bd09091f377e76f5d60c5832e0e91
Abdalla <aboode114g2@gmail.com>
2022-07-01 11:37:45 +0300

    creating 3 styled divs to split nav into 3 section

--- src/ECOMMERCE/components/NavbarComp.jsx
+++ src/ECOMMERCE/components/NavbarComp.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 import styled from "styled-components";
 
-//making styled components for NavbarComp
+//making styled components for NavbarComp. Capital
 const ContainerStyle = styled.div`
   height: 60px;
-  background-color: #000000;
+  background-color: #b4aaaa;
 `;
+const WrapperNavbar = styled.div`
+  padding: 10px 15px;
+`;
+//as creating 3 divs to split nav into 3 section
+const Left = styled.div``;
+const Center = styled.div``;
+const Right = styled.div``;
 const NavbarComp = () => {
   return (
     <ContainerStyle>
-      <h2>NavbarComp</h2>
+      <WrapperNavbar>
+        {/* <h2 style={{ color: "white", border: "1px solid yellow" }}>
+          NavbarComp
+        </h2> */}
+        <Left>Left</Left>
+        <Center>Cetner</Center>
+        <Right>right</Right>
+      </WrapperNavbar>
     </ContainerStyle>
   );
 };
