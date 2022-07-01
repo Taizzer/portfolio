bc73041a3ee8f2330e417ec20e3530b85040bc5a
Abdalla <aboode114g2@gmail.com>
2022-07-01 18:17:38 +0300

    array data

---
+++ src/ECOMMERCE/data.js
@@ -0,0 +1,41 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://d1fufvy4xao6k9.cloudfront.net/feed/img/woman_dress/73244/19FW_wed9_col5.png",
+    title: "SUMMER",
+    desc: "Women's Boho Floral Print Off Shoulder Split .",
+    bg: "ffd3d3",
+  },
+  {
+    id: 2,
+    img: "https://d1fufvy4xao6k9.cloudfront.net/feed/img/woman_dress/33821/front.png",
+    title: "AUTUMN ",
+    desc: "Women's Boho Floral Print Off Shoulder Split .",
+    bg: "d3fffd",
+  },
+  {
+    id: 3,
+    img: "https://d1fufvy4xao6k9.cloudfront.net/feed/img/woman_dress/34673/17SS_72_col.png",
+    title: " Winter",
+    desc: "Women's Boho Floral Print Off Shoulder Split .",
+    bg: "ffa3a3",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://m.media-amazon.com/images/I/81AQsMBicmL._AC_UX522_.jpg",
+    title: "SHIRT",
+  },
+  {
+    id: 2,
+    img: "https://m.media-amazon.com/images/I/61L+xygbp0S._AC_UX569_.jpg",
+    title: "PANTS",
+  },
+  {
+    id: 3,
+    img: "https://m.media-amazon.com/images/I/81ue5-58RmL._AC_UX679_.jpg",
+    title: "JACKETS",
+  },
+];
