ffdbf737fb10c5f6e29fdc0d351a699b54a8d519
Abdalla <aboode114g2@gmail.com>
2022-07-01 13:12:46 +0300

    removed  comp mistake

--- src/ECOMMERCE/ECOMMERCEAPP.jsx
+++ src/ECOMMERCE/ECOMMERCEAPP.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
+import HomePage from "./pages/HomePage";
+
+//rfc shortcut page starter or rafce
 const ECOMMERCEAPP = () => {
   return (
-    <div>
-      <h2>hi e com</h2>
+    <div style={{ paddingTop: 10 }}>
+      <h4>hi e com main app e-com</h4>
+
+      <HomePage />
     </div>
   );
 };
