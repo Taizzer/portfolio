36c3fdfc950a63b1b84b2b1d291730c4a6c6db46
Abdalla <aboode114g2@gmail.com>
2022-07-01 13:49:46 +0300

    slider comp added to homepage

--- src/ECOMMERCE/pages/HomePage.jsx
+++ src/ECOMMERCE/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import AnnouncementComp from "../components/AnnouncementComp";
 import NavbarComp from "../components/NavbarComp";
+import SliderComp from "../components/SliderComp";
 const HomePage = () => {
   return (
     <div>
       <AnnouncementComp />
       <NavbarComp />
+      <SliderComp />
     </div>
   );
 };
