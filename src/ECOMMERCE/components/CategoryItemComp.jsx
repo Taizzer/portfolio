e04ce3ad3d75d892edc9e15078893f563da23aa3
Abdalla <aboode114g2@gmail.com>
2022-07-01 18:24:58 +0300

    styled components image

--- src/ECOMMERCE/components/CategoryItemComp.jsx
+++ src/ECOMMERCE/components/CategoryItemComp.jsx
@@ -13,6 +13,15 @@ const Container = styled.div`
   border-radius: 12px;
 `;
 
+const Image = styled.img`
+  /* width: 100%;
+  height: 100%; */
+  /* our max- */
+  max-width: 250px;
+  max-height: 300px;
+  object-fit: cover;
+`;
+
 const CategoryItemComp = ({ item }) => {
   return (
     <Container>
