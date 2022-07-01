7a319e653c0796a7cf33a897aafc5742c18c958a
Abdalla <aboode114g2@gmail.com>
2022-07-01 21:04:09 +0300

    styled input

--- src/ECOMMERCE/components/NewsletterComp.jsx
+++ src/ECOMMERCE/components/NewsletterComp.jsx
@@ -12,15 +12,24 @@ const Container = styled.div`
 `;
 const Title = styled.h1`
   font-size: 64px;
-  margin-bottom: 18px;
-  font-size: 24px;
   font-weight: 400;
   margin-bottom: 18px;
 `;
 
-const Descri = styled.div``;
+const Descri = styled.div`
+  font-size: 24px;
+  font-weight: 300;
+  margin-bottom: 20px;
+`;
 
-const InputWrap = styled.div``;
+const InputWrap = styled.div`
+  width: 50%;
+  height: 40px;
+  background-color: white;
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid #c7c7c7;
+`;
 
 const Input = styled.input``;
 
