41b478100b05570f715d3ad2dbcbec6e4b4b0ad8
Abdalla <aboode114g2@gmail.com>
2022-07-01 13:59:09 +0300

    components

--- src/ECOMMERCE/components/SliderComp.jsx
+++ src/ECOMMERCE/components/SliderComp.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 import styled from "styled-components";
+import ArrowLeftOutlinedIcon from "@mui/icons-material/ArrowLeftOutlined";
+import ArrowRightOutlinedIcon from "@mui/icons-material/ArrowRightOutlined";
 
 const SliderComp = () => {
-  return <div>SliderComp</div>;
+  return (
+    <Container>
+      <Arrow direction="left">
+        <ArrowLeftOutlinedIcon />
+      </Arrow>
+      <Arrow direction="right">
+        <ArrowRightOutlinedIcon />
+      </Arrow>
+
+      <Wrap>
+        <Slide>
+          <ImgContainer>
+            <Image src="" />
+          </ImgContainer>
+
+          <InfoContainer></InfoContainer>
+        </Slide>
+      </Wrap>
+    </Container>
+  );
 };
 
 export default SliderComp;
