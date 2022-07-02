5b6f149ed8fef5a83fde8e7f1eb58a859577c2c4
Abdalla <aboode114g2@gmail.com>
2022-07-02 18:35:55 +0300

    components product

---
+++ src/ECOMMERCE/components/ProductComp.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import styled from "styled-components";
+
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+
+const Info = styled.div`
+  opacity: 0;
+  height: 100%;
+  width: 100%;
+
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.2);
+  z-index: 3;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.5s ease;
+  cursor: pointer;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex: 1;
+  margin: 5px;
+  align-items: center;
+  justify-content: center;
+  min-width: 280px;
+  height: 350px;
+  position: relative;
+  background-color: #f5fbfd;
+
+  /* on container hover show info icons */
+  &:hover ${Info} {
+    opacity: 1;
+  }
+`;
+
+const Image = styled.img`
+  height: 75%;
+  z-index: 2;
+`;
+
+const Icon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 10px;
+  transition: all 0.5s ease;
+  &:hover {
+    background-color: #e9f5f5;
+    transform: scale(1.1);
+  }
+`;
+
+const ProductComp = ({ item }) => {
+  return (
+    <Container>
+      <Image src={item.img} />
+      <Info>
+        <Icon>
+          <ShoppingCartOutlinedIcon />
+        </Icon>
+        <Icon>
+          <SearchOutlinedIcon />
+        </Icon>
+        <Icon>
+          <FavoriteBorderOutlinedIcon />
+        </Icon>
+      </Info>
+    </Container>
+  );
+};
+
+export default ProductComp;
