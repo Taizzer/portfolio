fd03afa5726fbef956cbee5ae51a46bc001dd355
Abdalla <aboode114g2@gmail.com>
2022-07-02 15:17:40 +0300

    add register comp

--- src/ECOMMERCE/ECOMMERCEAPP.jsx
+++ src/ECOMMERCE/ECOMMERCEAPP.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
 import HomePage from "./pages/HomePage";
+import ProductListPage from "./pages/ProductListPage";
+import ProductPage from "./pages/ProductPage";
+import RegisterPage from "./pages/RegisterPage";
 
 //rfc shortcut page starter or rafce
 const ECOMMERCEAPP = () => {
   return (
     <div style={{ paddingTop: 10 }}>
-      <h4>hi e com main app e-com</h4>
-
-      <HomePage />
+      {/* <HomePage /> */}
+      {/* <ProductListPage /> */}
+      {/* <ProductPage /> */}
+      <RegisterPage />
     </div>
   );
 };
