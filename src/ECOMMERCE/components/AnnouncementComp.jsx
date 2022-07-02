341eb72cff6f8e26a2fd19f24aa3741f14fcd4f8
Abdalla <aboode114g2@gmail.com>
2022-07-02 18:34:28 +0300

    components announce

--- src/ECOMMERCE/components/AnnouncementComp.jsx
+++ src/ECOMMERCE/components/AnnouncementComp.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
-  background: #687168;
-  color: #ffffff;
+  /* background: #979797; */
+
+  color: #10b19f;
   font-weight: 100;
   height: 25px;
+  /* border-bottom: 1px solid black; */
 `;
 const AnnouncementComp = () => {
   return (
