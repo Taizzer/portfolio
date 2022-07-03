f8141cd221733d5e1b0c121e8f76ec75b8a67416
Abdalla <aboode114g2@gmail.com>
2022-07-03 18:11:23 +0300

    html changed added github link

--- src/ForecastApp/ForecastApp.jsx
+++ src/ForecastApp/ForecastApp.jsx
@@ -5,27 +5,40 @@ import WeatherLondon from "./Components/WeatherLondon";
 import WeatherInput from "./Components/WeatherInput";
 const ForecastApp = () => {
   return (
-    <div className="ForecastApp">
-      <div>
-        <p className="weatherWrap">
-          github gggggggggfsdfsdfsdfsdgggggggssssssssss
-        </p>
-      </div>
-      <WeatherInput />
-      <WeatherLondon propquery="romania" />
-      <WeatherLondon propquery="london" />
+    <div>
+      <code
+        style={{
+          background: "white",
+          padding: "0.3rem",
+          borderRadius: "5px",
+          marginLeft: "10px",
+        }}
+      >
+        <a
+          target="_blank"
+          href="https://github.com/Taizzer/portfolio/tree/main/src/TrackShopApp"
+        >
+          https://github.com/Taizzer/portfolio/tree/main/src/ForecastApp
+        </a>
+      </code>
+      <div className="ForecastApp">
+        <div>{/* <p className="weatherWrap"></p> */}</div>
+        <WeatherInput />
+        <WeatherLondon propquery="romania" />
+        <WeatherLondon propquery="london" />
 
-      <WeatherLondon propquery="germany" />
-      <WeatherLondon propquery="netherland" />
-      <WeatherLondon propquery="france" />
-      <WeatherLondon propquery="spain" />
-      <WeatherLondon propquery="italy" />
-      <WeatherLondon propquery="ireland" />
-      <WeatherLondon propquery="sweden" />
-      <WeatherLondon propquery="denmark" />
-      <WeatherLondon propquery="belgium" />
-      <WeatherLondon propquery="california" />
-      <WeatherLondon propquery="miami" />
+        <WeatherLondon propquery="germany" />
+        <WeatherLondon propquery="netherland" />
+        <WeatherLondon propquery="france" />
+        <WeatherLondon propquery="spain" />
+        <WeatherLondon propquery="italy" />
+        <WeatherLondon propquery="ireland" />
+        <WeatherLondon propquery="sweden" />
+        <WeatherLondon propquery="denmark" />
+        <WeatherLondon propquery="belgium" />
+        <WeatherLondon propquery="california" />
+        <WeatherLondon propquery="miami" />
+      </div>
     </div>
   );
 };
