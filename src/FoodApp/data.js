137a86c94c7e3b3c9ba47e75bdd07517fd33f7ff
Abdalla <aboode114g2@gmail.com>
2022-07-03 18:08:13 +0300

    main data for items array

---
+++ src/FoodApp/data.js
@@ -0,0 +1,62 @@
+const menu = [
+  {
+    id: 1,
+    title: "Healthy tuna lettuce wraps",
+    price: 9.99,
+    category: "Breakfast",
+    img: "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/tuna-mayo-wraps-a1f5206.jpg?resize=960,872?quality=90&webp=true&resize=375,341",
+    desc: "Low-carb, high-protein lunch.",
+  },
+  {
+    id: 2,
+    title: "Quick vegetarian recipes",
+    price: 9.99,
+    category: "Breakfast",
+    img: "https://images.immediate.co.uk/production/volatile/sites/30/2013/05/PineappleFriedRice-e97ea07.jpg?resize=960,872?quality=90&webp=true&resize=375,341",
+    desc: "Sumptuous veggie meal .",
+  },
+  {
+    id: 3,
+    title: "BBQ",
+    price: 9.99,
+    category: "Lunch",
+    img: "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/bbq-rainbow-beef-salad-03d3bcf.jpg?resize=960,872?quality=90&webp=true&resize=375,341",
+    desc: "The steak with light salad .",
+  },
+  {
+    id: 4,
+    title: "Fresh salmon with Thai noodle salad",
+    price: 9.99,
+    category: "Diner",
+    img: "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/fresh-salmon-with-thai-noodle-salad-6977df3.jpg?resize=960,872?quality=90&webp=true&resize=375,341",
+    desc: "Salmon and noodle salad",
+  },
+  {
+    id: 5,
+    title: "Chocolate ice cream",
+    price: 9.99,
+    category: "Ice cream",
+    img: "https://images.immediate.co.uk/production/volatile/sites/30/2021/06/Next-Level_Ice-Cream-3db3bd9.jpg?resize=960,872?quality=90&webp=true&resize=375,341",
+    desc: "Fudgy chocolate ice cream.",
+  },
+  {
+    id: 6,
+    title: "Fruity Hoops Shake",
+    price: 9.99,
+    category: "Drinks",
+    img: "https://d2gtpjxvvd720b.cloudfront.net/system/recipe/image/2116/default_hg-healthy-fruity-hoops-shake-recipe-20170712-1428-29780-6816.jpg",
+    desc: "Calories 105,0.5g sugars, 20g protein",
+  },
+  {
+    id: 7,
+    title: "Sticky Bun Coffee",
+    price: 9.99,
+    category: "Drinks",
+    img: "https://d2gtpjxvvd720b.cloudfront.net/system/recipe/image/1605/default_Sticky-Bun-Coffee.jpg",
+    desc: "Calories 55 , 40mg sodium, 5g sugars",
+  },
+];
+
+// even the const name menu and the import is items...
+// because export default we name it as we wish when import
+export default menu;
