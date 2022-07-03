3bac4e32356be4be18bbbddbb29c55b1850f0302
Abdalla <aboode114g2@gmail.com>
2022-07-03 18:03:53 +0300

    fixed layout

--- src/components/AppNav/AppNav.jsx
+++ src/components/AppNav/AppNav.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./AppNav.css";
 import Inav1 from "../../imgs/Inav1.PNG";
+import weatherapi from "../../imgs/weatherapi.png";
+import foodmenu from "../../imgs/foodmenu.png";
+import trackchart from "../../imgs/trackchart.png";
+import ecom from "../../imgs/ecom.png";
 import { Link } from "react-router-dom";
 const MainNav = () => {
   return (
@@ -8,32 +12,32 @@ const MainNav = () => {
       <div className="MainNavWrap">
         <ul className="mainNavUl">
           <li>
-            <img src={Inav1} alt="" />
+            <img src={trackchart} alt="" />
             {/* <span>image1</span> */}
             <p>
-              <Link to="/">Home1</Link>
+              <Link to="/">Chart App</Link>
             </p>
           </li>
 
           <li>
-            <img src={Inav1} alt="" />
+            <img src={weatherapi} alt="" />
             {/* <span>image2</span> */}
             <p>
-              <Link to="del">del</Link>
+              <Link to="weatherapi">Weather Api</Link>
             </p>
           </li>
           <li>
-            <img src={Inav1} alt="" />
+            <img src={foodmenu} alt="" />
             {/* <span>image3</span> */}
             <p>
-              <Link to="shopapp">3</Link>
+              <Link to="foodApp">Restaurant</Link>
             </p>
           </li>
           <li>
-            <img src={Inav1} alt="" />
+            <img src={ecom} alt="" />
             {/* <span>image4</span> */}
             <p>
-              <Link to="test">4</Link>
+              <Link to="ecommerce">E-Commerce</Link>
             </p>
           </li>
         </ul>
