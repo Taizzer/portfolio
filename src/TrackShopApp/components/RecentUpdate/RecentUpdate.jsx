7eb13851fe5aaddf4bc1465166e4e7d2b9b85492
Abdalla <aboode114g2@gmail.com>
2022-10-03 17:05:26 +0300

    import/added welcome

--- src/TrackShopApp/components/RecentUpdate/RecentUpdate.jsx
+++ src/TrackShopApp/components/RecentUpdate/RecentUpdate.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./RecentUpdate.css";
 import { UpdateReview } from "../../../Data/Data.js";
+import Welcome from "../../../welcome/welcome";
 const RecentUpdate = () => {
   return (
     <div className="updates">
+      <Welcome />
       {UpdateReview.map((update) => {
         return (
           <div className="update">
